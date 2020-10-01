84080f6a55f06479468ba80190600d2f2b070f99
Kalinka George <xitowzys@gmail.com>
2020-10-01 20:32:47 +0800

    Initial Commit

---
+++ theFirstTenDigitPrime/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  theFirstTenDigitPrime
+//
+//  Created by xitowzys on 01.10.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
